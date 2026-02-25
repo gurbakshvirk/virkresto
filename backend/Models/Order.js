950d6ba03e17248664700a075e28f3b85a3ec576
gurbakshvirk <lovyvirk34@gmail.com>
2026-02-25 23:33:54 +0530

    added cart context

---
+++ backend/Models/Order.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  customer: {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    phone: {
+      type: String,
+      required: true
+    }
+  },
+
+  orderType: {
+    type: String,
+    enum: ["pickup", "delivery"],
+    required: true
+  },
+
+  // Only required if delivery
+  deliveryAddress: {
+    type: String
+  },
+
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      name: String,        // snapshot of product name
+      price: Number,       // snapshot price (important)
+      quantity: Number,
+      image: String        // optional snapshot
+    }
+  ],
+
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "preparing", "ready", "completed"],
+    default: "pending"
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Order", orderSchema);
